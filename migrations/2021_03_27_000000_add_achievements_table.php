9019c13b1b2ed9ebe584a63ac1071d6cf51d1988
Tassadar <lago@psych.ucsb.edu>
2021-03-27 18:13:28 -0400

    Fix migrations

--- migrations/2021_03_27_000000_add_achievements_table.php
+++ migrations/2021_03_27_000000_add_achievements_table.php
@@ -1,22 +1,10 @@
-<?
+<?php
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        $schema->create('achievement_user', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->integer('achievement_id')->unsigned();
-            $table->timestamps();
-            $table->boolean('new')->default(false);
-            
-            $table->primary(['user_id', 'achievement_id']);
-            
-            $table->foreign('achievement_id')->references('id')->on('achievements')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-        });
-
         $schema->create('achievements', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',200);
@@ -26,11 +14,19 @@ return [
             $table->string('image',255);
             $table->string('rectangle',100);
             $table->boolean('active')->default(true);
+        });
+
+        $schema->create('achievement_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('achievement_id')->unsigned();
+            $table->timestamps();
+            $table->boolean('new')->default(false);
             
-            $table->primary(['id']);
+            $table->index(['user_id', 'achievement_id']);
             
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-            
+            $table->foreign('achievement_id')->references('id')->on('achievements')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     },
     'down' => function (Builder $schema) {
